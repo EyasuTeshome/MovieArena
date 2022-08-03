72a481a5eb925685f7d80b816519bae24e75a984
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-08-03 16:33:31 +0300

    added es6 modules

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 /* eslint-disable */
 import './style.css';
+import Arr from './modules/displayAPI';
 
-fetch('https://yts.mx/api/v2/list_movies.json?genre=animation&limit=25&sort_by=download_count').then((data) => data.json()).then((objectData) => {
-  const Arr = objectData.data.movies;
-  let tableData = '';
-  Arr.map((value) => {
-    tableData += `<div class="mainlist">
-        <span><img src="${value.medium_cover_image}" alt=""></span>
-        <span class='heart-value'>${value.title} <i class="bi bi-heart">10</i></span>
-        <button class= "open-comments" >comments</button>
-      </div>
-      `;
-    document.getElementById('tableBody').innerHTML = tableData;
-  });
-});
+
