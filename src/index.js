9737575e03571bad82bcf663a09ff716a9e2c4b8
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-08-03 11:56:33 +0300

    displayed data from the API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import _ from 'lodash';
+/* eslint-disable */
 import './style.css';
 
+fetch('https://yts.mx/api/v2/list_movies.json?genre=animation&limit=25&sort_by=download_count').then((data) => data.json()).then((objectData) => {
+  const Arr = objectData.data.movies;
+  let tableData = '';
+  Arr.map((value) => {
+    tableData += `<ul class="p-5 d-flex flex-wrap gap-5 justify-content-center mainlist">
+        <li><img src="${value.background_image}" alt=""></li>
+        <li>${value.title}</li>
+      </ul>`;
+    document.getElementById('tableBody').innerHTML = tableData;
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,53 @@
+/* eslint-disable */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-    module: {
-        rules: [
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
           {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-              },
-            ],
+            loader: 'file-loader',
           },
         ],
       },
-
-    module: {
-        rules: [{
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        }, ],
-    },
-
-    devServer: {
-        static: './dist',
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
     ],
+  },
 
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/',
-    },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }],
+  },
 
-    optimization: {
-        runtimeChunk: 'single',
-    },
+  devServer: {
+    static: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
