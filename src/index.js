4a10451e912a8bc8e40531efb8e775347332fd56
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-08-03 16:06:03 +0300

    aligned the cards and added the comment and like

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ fetch('https://yts.mx/api/v2/list_movies.json?genre=animation&limit=25&sort_by=d
   const Arr = objectData.data.movies;
   let tableData = '';
   Arr.map((value) => {
-    tableData += `<ul class="p-5 d-flex flex-wrap gap-5 justify-content-center mainlist">
-        <li><img src="${value.background_image}" alt=""></li>
-        <li>${value.title}</li>
-      </ul>`;
+    tableData += `<div class="mainlist">
+        <span><img src="${value.medium_cover_image}" alt=""></span>
+        <span class='heart-value'>${value.title} <i class="bi bi-heart">10</i></span>
+        <button class= "open-comments" >comments</button>
+      </div>
+      `;
     document.getElementById('tableBody').innerHTML = tableData;
   });
 });
