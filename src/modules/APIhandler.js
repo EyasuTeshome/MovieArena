88a9a969b78cffa8633294e884748ab47b5fc798
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-08-06 16:42:53 +0300

    added the likes count to the app

--- src/modules/APIhandler.js
+++ src/modules/APIhandler.js
@@ -6,3 +6,21 @@ const fetchTVAPI = async () => {
   const getShowResult = await TVResponse.json();
   return getShowResult.slice(6, 18);
 };
+
+const fetchInvolvementAPI = async () => {
+    const involvementAPIResponse = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`);
+    const getLikeResult = await involvementAPIResponse.json();
+    return getLikeResult;
+  };
+  
+  const submitLike = async (newLike) => {
+    await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: newLike,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  };
