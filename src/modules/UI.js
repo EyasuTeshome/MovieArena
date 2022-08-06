8dfe0a0090c0de5cc7b06967a89f3c0fb0b495a0
Josh <joshtesh91@mail.com>
2022-08-06 21:26:43 +0800

    added comment popup for every movie in the list

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -8,6 +8,80 @@ const popUp = document.querySelector('.pop-up');
 const commentPopUp = document.querySelector('.comment');
 const showsContainer = document.querySelector('.shows-container');
 
+const openPopUpWindow = () => {
+  const commentButtons = document.getElementsByClassName('comment-btn');
+  Array.from(commentButtons).forEach((commentButton) => {
+    commentButton.addEventListener('click', async (e) => {
+      const showData = await fetchData.fetchTVAPI();
+      const targetId = e.target.id;
+      const commentData = await fetchData.fetchInvolvementAPIcomments(targetId);
+      showsContainer.classList.add('hide');
+      popUp.classList.remove('hide');
+      commentPopUp.classList.remove('hide');
+      const selectedShow = showData.filter(
+        (data) => data.id === Number(targetId),
+      )[0];
+
+      popUp.innerHTML = `<div class="display-popup-show">
+<p class='button_p'><button type="button" data-close-button class="close-button">&times;</button></p>
+         <div class="pop-up-img">  
+         <img src="${selectedShow.image.original}" alt="">
+           <h3>${selectedShow.name}</h3>
+           <p>${selectedShow.summary}</p>
+         </div>
+         <div class="detail_container">
+          <div>
+            <p><strong>Language: </strong> ${selectedShow.language}</p>
+            <p><strong>Premiered: </strong> ${selectedShow.premiered}</p>
+          </div>
+        <div>
+          <p><strong>Runtime: </strong>${selectedShow.runtime}</p>
+          <p><strong >Rating: </strong>${selectedShow.rating.average}</p>
+        </div>
+        </div>
+        <hr>
+        <div class="comment_container">
+          <h3 class="commnent-counter" >Comments(${commentCounter(
+    commentData,
+  )})</h3> 
+          <div class="comments-container">        
+          ${commentData
+    .map(
+      (data) => `
+        <span>${data.creation_date} </span>
+        <span>${data.username}: </span>
+        <span>${data.comment}</span><br>
+      `,
+    )
+    .join('')}
+    </div>
+    <hr>
+        </div>
+          <form action="#">
+          <input id="${targetId}" class="name_input" type="text" placeholder="Your name" name="username" required>
+          <input id="${targetId}" class="insight_input" type="text" placeholder="Your insights" name="insights" required>
+          <p class="button_p"><button class="submit_button btn btn-secondary comment-btn"  id="${targetId}" type="submit">Comment</button></p>
+        </form>
+        </div>`;
+      comments();
+    });
+  });
+};
+
+const closePopUp = () => {
+  const selector2 = '.close-button';
+
+  document.addEventListener('click', async (e) => {
+    const el = e.target;
+    if (!el.matches(selector2)) {
+      return;
+    }
+    showsContainer.classList.remove('hide');
+    popUp.classList.add('hide');
+    commentPopUp.classList.add('hide');
+  });
+};
+
 export const displayShows = async () => {
   const showData = await fetchData.fetchTVAPI();
   const involveData = await fetchData.fetchInvolvementAPI();
